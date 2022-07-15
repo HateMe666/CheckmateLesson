10ef5aafc4cf68a8e2072e0cf106b138b217419d
HateMe666 <nikita.kuzmich.11@gmail.com>
2022-07-15 17:36:48 +0300

    add checkers

--- StudentProject/ViewController.swift
+++ StudentProject/ViewController.swift
@@ -9,13 +9,97 @@ import UIKit
 
 class ViewController: UIViewController {
     
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = .green
         //        addStudents()
         //        namesArrayLabel()
         //        viewWithSmallViews()
-        checkmateBoard()
+        
+        view.addSubview(checkmateBoard(letters: addLettersArray(array: [Symbols]()), numbers: addNumbersArray(array: [Symbols]())))
+    }
+    
+    func addLettersArray(array: [Symbols]) -> [Symbols] {
+        return [Symbols(value: "A"), Symbols(value: "B"), Symbols(value: "C"), Symbols(value: "D"), Symbols(value: "E"), Symbols(value: "F"), Symbols(value: "G"), Symbols(value: "H"), Symbols(value: "M")]
+    }
+    
+    func addNumbersArray(array: [Symbols]) -> [Symbols] {
+        return [Symbols(value: "1"), Symbols(value: "2"), Symbols(value: "3"), Symbols(value: "4"), Symbols(value: "5"), Symbols(value: "6"), Symbols(value: "7"), Symbols(value: "8")]
+    }
+    
+    func addLabel(x: Double, y: Double, width: Double, height: Double, labelText: String) -> UILabel {
+        let label = UILabel(frame: CGRect(x: x , y: y, width: width, height: height))
+        label.backgroundColor = .yellow
+        label.text = labelText
+        label.textAlignment = .center
+        return label
+    }
+    
+    func addSqaures(x: Double, y: Double, width: Double, height: Double, color: UIColor) -> UIView {
+        let square = UIView(frame: CGRect(x: x, y: y, width: width, height: height))
+        square.backgroundColor = color
+        return square
+    }
+    func addChecker(x: Double, y: Double, width: Double, height: Double, color: UIColor) -> UIView {
+        let checker = UIView(frame: CGRect(x: x, y: y, width: width, height: height))
+        checker.backgroundColor = color
+        checker.layer.cornerRadius = 7
+        return checker
+    }
+    
+    func checkmateBoard(letters: [Symbols], numbers: [Symbols]) -> UIView {
+        let boardView = UIView(frame: CGRect(x: 10, y: 150, width: 300, height: 300))
+        boardView.backgroundColor = .brown
+        
+        let letterLabelX = 0.0
+        let letterLabelY = 0.0
+        let sizeLabel = Double(200 / ((letters.count + numbers.count) / 2))
+        let sizeSquare = sizeLabel
+        
+        for indexDown in 0...numbers.count - 1 {
+            for indexRight in 0...letters.count - 1 {
+                if (indexDown + indexRight) % 2 == 0 {
+                    let blackSquare = addSqaures(x: sizeSquare * Double(indexRight) + 50, y:  sizeSquare * Double(indexDown) + 50, width: sizeSquare, height: sizeSquare, color: .black)
+                    boardView.addSubview(blackSquare)
+                    if indexDown < 3 {
+                        blackSquare.addSubview(addChecker(x: sizeSquare / 4, y: sizeSquare / 4, width: sizeSquare / 2, height: sizeSquare / 2, color: .red))
+                    }
+                    if indexDown > numbers.count - 4 {
+                        blackSquare.addSubview(addChecker(x: sizeSquare / 4, y: sizeSquare / 4, width: sizeSquare / 2, height: sizeSquare / 2, color: .blue))
+                    }
+                }
+                else {
+                    boardView.addSubview(addSqaures(x: sizeSquare * Double(indexRight) + 50, y: sizeSquare * Double(indexDown) + 50, width: sizeSquare, height: sizeSquare, color: .white))
+                }
+            }
+        }
+        
+        for lettersIndex in 0...letters.count - 1 {
+            if letters.count % 2 == 0 && numbers.count % 2 == 1 {
+                boardView.addSubview(addLabel(x: sizeLabel * Double(lettersIndex) + 50, y: letterLabelY + 50 - sizeLabel / 2, width: sizeSquare, height: sizeSquare / 2, labelText: letters[lettersIndex].value))
+                
+                boardView.addSubview(addLabel(x: sizeLabel * Double(lettersIndex) + 50, y: 250 + sizeLabel, width: sizeSquare, height: sizeSquare / 2, labelText: letters[lettersIndex].value))
+            } else {
+                boardView.addSubview(addLabel(x: sizeLabel * Double(lettersIndex) + 50, y: letterLabelY + 50 - sizeLabel / 2, width: sizeSquare, height: sizeSquare / 2, labelText: letters[lettersIndex].value))
+                
+                boardView.addSubview(addLabel(x: sizeLabel * Double(lettersIndex) + 50, y: 250, width: sizeSquare, height: sizeSquare / 2, labelText: letters[lettersIndex].value))
+            }
+        }
+        
+        for numbersIndex in 0...numbers.count - 1 {
+            if numbers.count % 2 == 0 && letters.count % 2 == 1 {
+                boardView.addSubview(addLabel(x: letterLabelX + 50 - sizeLabel / 2, y: sizeLabel * Double(numbersIndex) + 50, width: sizeSquare / 2, height: sizeSquare, labelText: numbers[numbersIndex].value))
+                
+                boardView.addSubview(addLabel(x: 250 + sizeLabel, y: sizeLabel * Double(numbersIndex) + 50, width: sizeSquare / 2, height: sizeSquare, labelText: numbers[numbersIndex].value))
+            } else {
+                boardView.addSubview(addLabel(x: letterLabelX + 50 - sizeLabel / 2, y: sizeLabel * Double(numbersIndex) + 50, width: sizeSquare / 2, height: sizeSquare, labelText: numbers[numbersIndex].value))
+                
+                boardView.addSubview(addLabel(x: 250, y: sizeLabel * Double(numbersIndex) + 50, width: sizeSquare / 2, height: sizeSquare, labelText: numbers[numbersIndex].value))
+            }
+        }
+        return boardView
     }
     
     func addStudents() {
@@ -65,65 +149,16 @@ class ViewController: UIViewController {
         }
     }
     
-    func checkmateBoard() {
-        let boardView = UIView(frame: CGRect(x: 0, y: 100, width: 350, height: 350))
-        boardView.backgroundColor = .brown
-        view.addSubview(boardView)
-        
-        let letterLabelX = 10.0
-        let letterLabelY = 10.0
-        let sizeSquare = 31.25
-        let sizeLabel = 31.25
-        let lettersArray = [LettersAndNumbers(letter: "A"), LettersAndNumbers(letter: "B"), LettersAndNumbers(letter: "C"), LettersAndNumbers(letter: "D"), LettersAndNumbers(letter: "E"), LettersAndNumbers(letter: "F"), LettersAndNumbers(letter: "G"), LettersAndNumbers(letter: "H")]
-        
-        let numbersArray = [LettersAndNumbers(letter: "1"),LettersAndNumbers(letter: "2"),LettersAndNumbers(letter: "3"),LettersAndNumbers(letter: "4"),LettersAndNumbers(letter: "5"),LettersAndNumbers(letter: "6"),LettersAndNumbers(letter: "7"),LettersAndNumbers(letter: "8")]
-        
-        for indexDown in 1...8 {
-            for indexRight in 1...8 {
-                if (indexDown + indexRight) % 2 == 0 {
-                    let blackSquare = UIView(frame: CGRect(x: sizeSquare * Double(indexRight), y: sizeSquare * Double(indexDown), width: sizeSquare, height: sizeSquare))
-                    blackSquare.backgroundColor = .black
-                    boardView.addSubview(blackSquare)
-                } else {
-                    let whiteSquare = UIView(frame: CGRect(x: sizeSquare * Double(indexRight), y: sizeSquare * Double(indexDown), width: sizeSquare, height: sizeSquare))
-                    whiteSquare.backgroundColor = .white
-                    boardView.addSubview(whiteSquare)
-                }
-            }
-            let lettersLabel = UILabel(frame: CGRect(x: sizeLabel * Double(indexDown) , y: letterLabelY, width: sizeLabel, height: sizeLabel / 2))
-            lettersLabel.backgroundColor = .yellow
-            boardView.addSubview(lettersLabel)
-            lettersLabel.text = lettersArray[indexDown - 1].letter
-            lettersLabel.textAlignment = .center
-            
-            let numbersLabel = UILabel(frame: CGRect(x: letterLabelX  , y: sizeLabel * Double(indexDown), width: sizeLabel / 2, height: sizeLabel))
-            numbersLabel.backgroundColor = .yellow
-            boardView.addSubview(numbersLabel)
-            numbersLabel.text = numbersArray[indexDown - 1].letter
-            numbersLabel.textAlignment = .center
-            
-            let lettersLabelRight = UILabel(frame: CGRect(x: sizeLabel * Double(indexDown) , y: 285 , width: sizeLabel, height: sizeLabel / 2))
-            lettersLabelRight.backgroundColor = .yellow
-            boardView.addSubview(lettersLabelRight)
-            lettersLabelRight.text = lettersArray[indexDown - 1].letter
-            lettersLabelRight.textAlignment = .center
-            
-            let numbersLabelDown = UILabel(frame: CGRect(x: 285  , y: sizeLabel * Double(indexDown), width: sizeLabel / 2, height: sizeLabel))
-            numbersLabelDown.backgroundColor = .yellow
-            boardView.addSubview(numbersLabelDown)
-            numbersLabelDown.text = numbersArray[indexDown - 1].letter
-            lettersLabelRight.textAlignment = .center
-        }
+}
+
+class Symbols {
+    var value: String
+    
+    init(value: String) {
+        self.value = value
     }
 }
 
-class LettersAndNumbers {
-    var letter: String
-    
-    init(letter: String) {
-        self.letter = letter
-    }
-}
 
 
 class Student {
@@ -133,6 +168,8 @@ class Student {
         self.name = name
     }
 }
+
+
 
 
 // Массив учеников, на каждого ученика свой лейбл со своим именем
